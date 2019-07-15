97ed1f41fb0482bfeee72273d93538f0b2d42b66
Dave Maynard <davemaynard24@gmail.com>
2019-07-15 13:53:13 -0400

    question 15 solution

---
+++ questions-001-100/question-015/question-015.test.js
@@ -0,0 +1,41 @@
+// Lattice paths
+
+// Problem 15
+// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner. Refer to the image here: https://projecteuler.net/problem=15.
+
+// How many such routes are there through a 20×20 grid?
+
+function getRoutes(width) {
+  let nodesPerSide = width + 1,
+    rows = [];
+
+  for (let i = 0; i < nodesPerSide; i++) {
+    let row = [];
+    for (let j = 0; j < nodesPerSide; j++) {
+      if (i == nodesPerSide - 1) {
+        row.push(1);
+      } else {
+        if (j == nodesPerSide - 1) {
+          row.push(1);
+        } else {
+          row.push(0);
+        }
+      }
+    }
+    rows.push(row);
+  }
+
+  for (let y = width - 1; y > -1; y--) {
+    for (let x = y; x > -1; x--) {
+      let nodeValue = rows[y][x + 1] + rows[y + 1][x];
+      rows[y][x] = nodeValue;
+      rows[x][y] = nodeValue;
+    }
+  }
+
+  return rows[0][0];
+}
+
+test('gets the number of routes through a 20×20 grid to be 137846528820', () => {
+  expect(getRoutes(20)).toBe(137846528820);
+});

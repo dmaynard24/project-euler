74792c1ac94b6670d10d5ff1d2c5d947971c1970
Dave Maynard <davemaynard24@gmail.com>
2019-07-18 13:49:12 -0400

    question 22 solution

--- questions-001-100/question-021/question-021.test.js
+++ questions-001-100/question-021/question-021.test.js
@@ -43,6 +43,6 @@ function d(num) {
   return getProperDivisors(num).reduce((a, c) => a + c);
 }
 
-test('gets the sum of all the amicable numbers under 10000 be 31626', () => {
+test('gets the sum of all the amicable numbers under 10000 to be 31626', () => {
   expect(getAmicableSum(10000)).toBe(31626);
 });

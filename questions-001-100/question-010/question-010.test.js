a9721848ab50e6fb88688cfc2b142c45b7b03827
Dave Maynard <davemaynard24@gmail.com>
2019-08-07 18:16:27 -0400

    refactored question 10 with a more efficient prime sieve

--- questions-001-100/question-010/question-010.test.js
+++ questions-001-100/question-010/question-010.test.js
@@ -6,21 +6,26 @@
 // Find the sum of all the primes below two million.
 
 function getPrimeSum(limit) {
-  return getPrimes(limit).reduce((a, c, i) => (c ? a + i : a), 0);
+  let primes = getPrimes(limit),
+    sum = 0;
+  for (let i = 2; i < limit; i++) {
+    if (isPrime(i, primes)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 // primes using Sieve of Eratosthenes
 function getPrimes(limit) {
-  let primes = Array(limit + 1).fill(true);
+  let oddsOnlyLimit = Math.floor(limit / 2) + 1;
+  let primes = Array(oddsOnlyLimit).fill(true);
 
-  primes[0] = false;
-  primes[1] = false;
-
-  let step = 2;
-  for (let i = 2; i <= Math.sqrt(limit); i++) {
+  for (let i = 1; i <= Math.sqrt(limit); i++) {
+    let n = 2 * i + 1;
     if (primes[i]) {
-      step = i;
-      for (let j = step * step; j <= limit; j += step) {
+      let step = n;
+      for (let j = step == 3 ? i + step : i + step * 2; j <= oddsOnlyLimit; j += step) {
         primes[j] = false;
       }
     }
@@ -28,6 +33,18 @@ function getPrimes(limit) {
 
   return primes;
 }
+
+function isPrime(n, primes) {
+  if (n % 2 == 0) {
+    return n == 2;
+  }
+
+  return primes[(n - 1) / 2];
+}
+
+test('gets the sum of all the primes below ten to be 17', () => {
+  expect(getPrimeSum(10)).toBe(17);
+});
 
 test('gets the sum of all the primes below two million to be 142913828922', () => {
   expect(getPrimeSum(2000000)).toBe(142913828922);

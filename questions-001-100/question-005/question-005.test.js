ee7893233727a1d843a4e0b739925fcf0c36ce49
Dave Maynard <davemaynard24@gmail.com>
2019-08-18 17:15:35 -0400

    refactored question 5 solution to be more readable. also fixed a bug in the getPrimeFactors function

--- questions-001-100/question-005/question-005.test.js
+++ questions-001-100/question-005/question-005.test.js
@@ -6,39 +6,74 @@
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 function getSmallestMultiple(upper) {
-  let all = [...Array(upper + 1).keys()].splice(1);
+  let nums = [...Array(upper + 1).keys()].splice(1),
+    primeFactorCounts = [],
+    primes = getPrimes(16000);
 
-  let npFactorCounts = all
-    .map(np => getPrimeFactors(np))
-    .map(npfs => {
-      return npfs.reduce((acc, curr) => {
-        acc[curr] ? acc[curr]++ : (acc[curr] = 1);
-        return acc;
-      }, {});
-    })
-    .reduce((acc, curr) => {
-      Object.keys(curr).forEach(key => {
-        if (!acc[key] || acc[key] < curr[key]) {
-          acc[key] = curr[key];
-        }
+  nums.forEach(num => {
+    if (isPrime(num, primes)) {
+      primeFactorCounts[num] = 1;
+    } else {
+      let pfs = getPrimeFactors(num, primes);
+      pfs.forEach(pf => {
+        primeFactorCounts[pf.base] = primeFactorCounts[pf.base] ? Math.max(primeFactorCounts[pf.base], pf.exp) : pf.exp;
       });
-      return acc;
-    }, {});
+    }
+  });
 
-  let lcm = Object.keys(npFactorCounts).reduce((acc, curr) => {
-    acc *= Math.pow(curr, npFactorCounts[curr]);
+  let lcm = Object.keys(primeFactorCounts).reduce((acc, curr) => {
+    acc *= Math.pow(curr, primeFactorCounts[curr]);
     return acc;
   }, 1);
 
   return lcm;
 }
 
-function getPrimeFactors(num) {
-  let factors = [];
+// primes using Sieve of Eratosthenes (storing only odds)
+function getPrimes(limit) {
+  let oddsOnlyLimit = Math.floor(limit / 2) + 1,
+    primes = Array(oddsOnlyLimit).fill(true);
 
-  while (num % 2 == 0) {
-    factors.push(2);
-    num /= 2;
+  primes[0] = false;
+
+  for (let i = 1; i <= Math.sqrt(limit); i++) {
+    let n = 2 * i + 1;
+    if (primes[i]) {
+      let step = n;
+      for (let j = step == 3 ? i + step : i + step * 2; j <= oddsOnlyLimit; j += step) {
+        primes[j] = false;
+      }
+    }
+  }
+
+  return primes;
+}
+
+function isPrime(n, primes) {
+  if (n % 2 == 0) {
+    return n == 2;
+  }
+
+  return primes[(n - 1) / 2];
+}
+
+function getPrimeFactors(num, primes) {
+  let primeFactors = [];
+
+  if (isPrime(num, primes)) {
+    return primeFactors;
+  }
+
+  if (num % 2 == 0) {
+    let baseExp = {
+      base: 2,
+      exp: 0
+    };
+    while (num % 2 == 0) {
+      baseExp.exp++;
+      num /= 2;
+    }
+    primeFactors.push(baseExp);
   }
 
   let sqrt = Math.sqrt(num);
@@ -46,18 +81,34 @@ function getPrimeFactors(num) {
     if (num < i) {
       break;
     }
-    while (num % i == 0) {
-      factors.push(i);
-      num /= i;
+    if (isPrime(i, primes)) {
+      if (num % i == 0) {
+        let baseExp = {
+          base: i,
+          exp: 0
+        };
+        while (num % i == 0) {
+          baseExp.exp++;
+          num /= i;
+        }
+        primeFactors.push(baseExp);
+      }
     }
   }
 
   if (num > 2) {
-    factors.push(num);
+    primeFactors.push({
+      base: num,
+      exp: 1
+    });
   }
 
-  return factors;
+  return primeFactors;
 }
+
+test('gets the smallest positive number that is evenly divisible by 1 through 10 to be 2520', () => {
+  expect(getSmallestMultiple(10)).toBe(2520);
+});
 
 test('gets the smallest positive number that is evenly divisible by 1 through 20 to be 232792560', () => {
   expect(getSmallestMultiple(20)).toBe(232792560);

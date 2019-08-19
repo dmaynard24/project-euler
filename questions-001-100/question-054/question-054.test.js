f695230028db18e95a3cb25cdd586a967779ddb4
Dave Maynard <davemaynard24@gmail.com>
2019-08-19 13:56:41 -0400

    added a second test case to question 54

--- questions-001-100/question-054/question-054.test.js
+++ questions-001-100/question-054/question-054.test.js
@@ -1290,3 +1290,7 @@ function getConsecutiveCountsAndKicker(ranks) {
 test('gets the count of hands that Player 1 won to be 376', () => {
   expect(getHandsPlayerWon(1)).toBe(376);
 });
+
+test('gets the count of hands that Player 2 won to be 624', () => {
+  expect(getHandsPlayerWon(2)).toBe(624);
+});

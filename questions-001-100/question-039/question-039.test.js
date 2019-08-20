8ad64c4e7c32bd10d1f264b1b6e6aa66dc416b40
Dave Maynard <davemaynard24@gmail.com>
2019-08-20 17:40:22 -0400

    optimized function to get primitive pythagorean triples

--- questions-001-100/question-039/question-039.test.js
+++ questions-001-100/question-039/question-039.test.js
@@ -35,7 +35,8 @@ function getPrimitiveTriples(pMax) {
 
   // by looking at the values of b and c below, I'm able to limit the range of m based on the pMax
   for (let m = 2; m * m + 2 * m < pMax; m++) {
-    for (let n = 1; n < m; n++) {
+    let start = (m % 2) + 1;
+    for (let n = start; n < m; n += 2) {
       let a = m * m - n * n,
         b = 2 * m * n,
         c = m * m + n * n,

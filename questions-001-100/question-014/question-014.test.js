d3b8bad8da97461dc0ac2c026efa76267ac7514f
Dave Maynard <davemaynard24@gmail.com>
2019-08-21 15:44:13 -0400

    wrote separate tests for questions 11 - 15

--- questions-001-100/question-014/question-014.test.js
+++ questions-001-100/question-014/question-014.test.js
@@ -1,43 +1,4 @@
-// Longest Collatz sequence
-
-// Problem 14
-// The following iterative sequence is defined for the set of positive integers:
-
-// n → n/2 (n is even)
-// n → 3n + 1 (n is odd)
-
-// Using the rule above and starting with 13, we generate the following sequence:
-
-// 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-// It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
-
-// Which starting number, under one million, produces the longest chain?
-
-// NOTE: Once the chain starts the terms are allowed to go above one million.
-
-function getLargestCollatzSequence(limit) {
-  let start = 2,
-    largestSequenceStart = 2,
-    largestCount = 1;
-  while (start < limit) {
-    let term = start,
-      count = 1;
-    while (term != 1) {
-      if (term % 2 == 0) {
-        term /= 2;
-      } else {
-        term = 3 * term + 1;
-      }
-      count++;
-    }
-    if (count > largestCount) {
-      largestCount = count;
-      largestSequenceStart = start;
-    }
-    start++;
-  }
-  return largestSequenceStart;
-}
+const getLargestCollatzSequence = require('./question-014');
 
 test('gets the starting number under one million that produces the longest Collatz chain to be 837799', () => {
   expect(getLargestCollatzSequence(1000000)).toBe(837799);

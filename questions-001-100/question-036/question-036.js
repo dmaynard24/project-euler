1a4aa020276827c38ceaf2da8387f275aa986db4
Dave Maynard <davemaynard24@gmail.com>
2019-08-24 14:19:37 -0400

    separated question 36 test into a module

---
+++ questions-001-100/question-036/question-036.js
@@ -0,0 +1,30 @@
+// Double-base palindromes
+
+// Problem 36
+// The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+// Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+// (Please note that the palindromic number, in either base, may not include leading zeros.)
+
+function getPalindromicSum(limit) {
+  let sum = 0;
+
+  for (let i = 1; i < limit; i += 2) {
+    if (isPalindrome(i)) {
+      let binary = i.toString(2);
+      if (isPalindrome(binary)) {
+        sum += i;
+      }
+    }
+  }
+
+  return sum;
+}
+
+function isPalindrome(val) {
+  let str = val.toString();
+  return str == [...str].reverse().join('');
+}
+
+module.exports = getPalindromicSum;
--- questions-001-100/question-036/question-036.test.js
+++ questions-001-100/question-036/question-036.test.js
@@ -1,31 +1,4 @@
-// Double-base palindromes
-
-// Problem 36
-// The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
-
-// Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
-
-// (Please note that the palindromic number, in either base, may not include leading zeros.)
-
-function getPalindromicSum(limit) {
-  let sum = 0;
-
-  for (let i = 1; i < limit; i += 2) {
-    if (isPalindrome(i)) {
-      let binary = i.toString(2);
-      if (isPalindrome(binary)) {
-        sum += i;
-      }
-    }
-  }
-
-  return sum;
-}
-
-function isPalindrome(val) {
-  let str = val.toString();
-  return str == [...str].reverse().join('');
-}
+const getPalindromicSum = require('./question-036');
 
 test('gets the sum of all numbers, less than one million, which are palindromic in base 10 and base 2 to be 872187', () => {
   expect(getPalindromicSum(1000000)).toBe(872187);

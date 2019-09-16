d46171cc94865c05d8035a1fd08b0cb5842c99b6
Dave Maynard <davemaynard24@gmail.com>
2019-09-16 10:27:46 -0400

    reordered lines in solution 64 for better readability

--- questions-001-100/question-064/question-064.js
+++ questions-001-100/question-064/question-064.js
@@ -34,21 +34,22 @@ function getPeriod(num) {
     m = Math.floor(sqrt),
     int = m,
     n = sqrt + int,
-    d = num - m * m,
+    d = num - int * int,
     fraction = n / d,
     ms = [];
 
   while (d != 1) {
     // get new m
     m = Math.floor(fraction);
-    ms.push(m);
-
     // take reciprocal, then remove sqrt from denominator and simplify
     int -= d * m;
     int *= -1;
     n = sqrt + int;
     d = (num - int * int) / d;
     fraction = n / d;
+
+    // store m
+    ms.push(m);
   }
 
   ms.push(Math.floor(sqrt) * 2);

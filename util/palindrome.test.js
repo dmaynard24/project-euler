8a18d368ad6b360e8968ee0adb7f90103bd9e1c1
Dave Maynard <davemaynard24@gmail.com>
2019-10-09 16:20:40 -0400

    updated palindrome test module

--- util/palindrome.test.js
+++ util/palindrome.test.js
@@ -1,9 +1,9 @@
-const isPalindrome = require('./palindrome');
+const palindrome = require('./palindrome');
 
 test('expects that 394493 is a palindrome', () => {
-  expect(isPalindrome(394493)).toBe(true);
+  expect(palindrome.isPalindrome(394493)).toBe(true);
 });
 
 test('expects that 394463 is not a palindrome', () => {
-  expect(isPalindrome(394463)).toBe(false);
+  expect(palindrome.isPalindrome(394463)).toBe(false);
 });

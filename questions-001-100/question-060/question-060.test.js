efd33a58b2dd415498d9391bcbf90241fc75b1b9
Dave Maynard <davemaynard24@gmail.com>
2019-10-31 10:22:52 -0400

    wrote another test for question 60

--- questions-001-100/question-060/question-060.test.js
+++ questions-001-100/question-060/question-060.test.js
@@ -1,5 +1,9 @@
 const getLowestPrimeSum = require('./question-060');
 
-test('gets the lowest sum for a set of four primes for which any two primes concatenate to produce another prime to be 792', () => {
+test('gets the lowest sum for a set of 4 primes for which any two primes concatenate to produce another prime to be 792', () => {
   expect(getLowestPrimeSum(4)).toBe(792);
 });
+
+// test('gets the lowest sum for a set of 5 primes for which any two primes concatenate to produce another prime to be 26033', () => {
+//   expect(getLowestPrimeSum(5)).toBe(26033);
+// });

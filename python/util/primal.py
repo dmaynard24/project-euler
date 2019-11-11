be04342f15027538cb9a5d3f4b054604a7dea63c
Dave Maynard <davemaynard24@gmail.com>
2019-11-10 23:24:59 -0500

    ported most of my primal utility module into python. still need to figure out how to impoort it from other directories

---
+++ python/util/primal.py
@@ -0,0 +1,62 @@
+import math
+
+
+# primes using Sieve of Eratosthenes (storing only odds)
+def get_primes(limit):
+  odds_only_limit = limit // 2 + 1
+  primes = [True] * odds_only_limit
+  primes[0] = False
+  for i in range(1, int(math.sqrt(limit))):
+    n = 2 * i + 1
+    if primes[i]:
+      step = n
+      start = i + step if step == 3 else i + step * 2
+      for j in range(start, odds_only_limit, step):
+        primes[j] = False
+  return primes
+
+
+def is_prime(num, primes=None):
+  if primes is not None and len(primes) >= num:
+    if num % 2 == 0:
+      return num == 2
+    return primes[(num - 1) / 2]
+  # primes (sieve) wasn't passed as an argument, naive check if n is prime
+  if num < 1:
+    return True
+  elif num == 2 or num == 3:
+    return True
+  elif num % 2 == 0 or num % 3 == 0:
+    return False
+  i = 5
+  while i * i <= num:
+    if num % i == 0 or num % (i + 2) == 0:
+      return False
+    i += 6
+  return True
+
+
+def get_prime_factors(num, primes):
+  if is_prime(num, primes):
+    return []
+  prime_factors = []
+  if (num % 2 == 0):
+    base_exp = {'base': 2, 'exp': 0}
+    while num % 2 == 0:
+      base_exp['exp'] += 1
+      num /= 2
+    prime_factors.append(base_exp)
+  sqrt = math.sqrt(num)
+  for i in range(3, int(sqrt), 2):
+    if num < i:
+      break
+    if is_prime(i, primes):
+      if num % i == 0:
+        base_exp = {'base': i, 'exp': 0}
+        while num % i == 0:
+          base_exp['exp'] += 1
+          num /= i
+        prime_factors.append(base_exp)
+  if num > 2:
+    prime_factors.append({'base': num, 'exp': 1})
+  return prime_factors

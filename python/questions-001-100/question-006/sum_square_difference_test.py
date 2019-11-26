3dcab61e92b9cd8e7a39a4464f9ca38204db5be1
Dave Maynard <davemaynard24@gmail.com>
2019-11-26 14:31:28 -0500

    ported solution 6 to python

---
+++ python/questions-001-100/question-006/sum_square_difference_test.py
@@ -0,0 +1,11 @@
+import unittest, sum_square_difference
+
+
+class TestGetSumSquareDifference(unittest.TestCase):
+  def test_get_sum_square_difference(self):
+    self.assertEqual(sum_square_difference.get_sum_square_difference(100),
+                     25164150)
+
+
+if __name__ == '__main__':
+  unittest.main()

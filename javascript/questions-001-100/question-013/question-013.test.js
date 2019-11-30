e89d890dab609fd0a3adf6e9143e51d6d02219b2
Dave Maynard <davemaynard24@gmail.com>
2019-11-29 20:52:23 -0500

    refactored the logic around solution 13 (js)

--- javascript/questions-001-100/question-013/question-013.test.js
+++ javascript/questions-001-100/question-013/question-013.test.js
@@ -1,5 +1,5 @@
 const getFirstDigitsOfSum = require('./question-013');
 
 test('gets the first ten digits of the sum of the one-hundred 50-digit numbers to be 5537376230', () => {
-  expect(getFirstDigitsOfSum(10)).toBe('5537376230');
+  expect(getFirstDigitsOfSum(10)).toBe(5537376230);
 });

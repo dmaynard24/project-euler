f6e5d9a3ec1b08f65a153474575e73121182e38a
Dave Maynard <davemaynard24@gmail.com>
2019-11-29 21:32:55 -0500

    ported solution 13 to python

---
+++ python/questions-001-100/question-013/first_digits_of_sum_test.py
@@ -0,0 +1,11 @@
+import unittest, first_digits_of_sum
+
+
+class TestGetFirstDigitsOfSum(unittest.TestCase):
+  def test_get_first_digits_of_sum_one(self):
+    self.assertEqual(first_digits_of_sum.get_first_digits_of_sum(10),
+                     5537376230)
+
+
+if __name__ == '__main__':
+  unittest.main()

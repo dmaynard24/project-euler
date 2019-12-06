ef6f8a1af991448e1989186e3983bfbf55daf060
Dave Maynard <davemaynard24@gmail.com>
2019-12-06 16:58:39 -0500

    ported solution 20 to python

---
+++ python/questions-001-100/question-020/factorial_digit_sum.py
@@ -0,0 +1,23 @@
+# Factorial digit sum
+
+# Problem 20
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+# Find the sum of the digits in the number 100!
+
+import sys, os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_dir)
+
+from python.util import digits
+
+
+def get_factorial_digit_sum(num):
+  factorial = 1
+  for i in range(2, num + 1):
+    factorial *= i
+
+  return sum(digits.get_digits_reversed(factorial))
---
+++ python/questions-001-100/question-020/factorial_digit_sum_test.py
@@ -0,0 +1,15 @@
+import unittest, factorial_digit_sum
+
+
+class TestGetFactorialDigitSumOne(unittest.TestCase):
+  def test_get_factorial_digit_sum(self):
+    self.assertEqual(factorial_digit_sum.get_factorial_digit_sum(10), 27)
+
+
+class TestGetFactorialDigitSumTwo(unittest.TestCase):
+  def test_get_factorial_digit_sum(self):
+    self.assertEqual(factorial_digit_sum.get_factorial_digit_sum(100), 648)
+
+
+if __name__ == '__main__':
+  unittest.main()

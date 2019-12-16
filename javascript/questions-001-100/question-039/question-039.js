d863094918102cf89ceba401b53ccc2ab50d5087
Dave Maynard <davemaynard24@gmail.com>
2019-12-16 17:10:44 -0500

    referenced new util module in solution 39

--- javascript/questions-001-100/question-039/question-039.js
+++ javascript/questions-001-100/question-039/question-039.js
@@ -7,7 +7,7 @@
 
 // For which value of p ≤ 1000, is the number of solutions maximised?
 
-const areCoprime = require('../../util/coprime');
+const getPrimitiveTriples = require('../../util/pythagorean-triples');
 
 function getMostCommonPerimeter(pMax) {
   let primitives = getPrimitiveTriples(pMax),
@@ -30,36 +30,6 @@ function getMostCommonPerimeter(pMax) {
   });
 
   return mostCommon;
-}
-
-function getPrimitiveTriples(pMax) {
-  let triples = [];
-
-  // by looking at the values of b and c below, I'm able to limit the range of m based on the pMax
-  for (let m = 2; m * m + 2 * m < pMax; m++) {
-    let start = (m % 2) + 1;
-    for (let n = start; n < m; n += 2) {
-      let a = m * m - n * n,
-        b = 2 * m * n,
-        c = m * m + n * n,
-        p = a + b + c;
-
-      if (p > pMax) {
-        break;
-      }
-
-      if (areCoprime(a, b)) {
-        triples.push({
-          a: a,
-          b: b,
-          c: c,
-          p: p
-        });
-      }
-    }
-  }
-
-  return triples;
 }
 
 module.exports = getMostCommonPerimeter;

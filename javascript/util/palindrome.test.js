a081d4d5c83a1f5a71a4e65c1dd263d8806ab759
Dave Maynard <davemaynard24@gmail.com>
2019-12-19 11:03:57 -0500

    updated palindrome util test suite

--- javascript/util/palindrome.test.js
+++ javascript/util/palindrome.test.js
@@ -4,12 +4,20 @@ test('expects that 394493 is a palindrome', () => {
   expect(palindrome.isPalindrome(394493)).toBe(true);
 });
 
+test('expects that 39493 is a palindrome', () => {
+  expect(palindrome.isPalindrome(39493)).toBe(true);
+});
+
 test('expects that 394463 is not a palindrome', () => {
   expect(palindrome.isPalindrome(394463)).toBe(false);
 });
 
 test('expects that [3, 9, 4, 4, 9, 3] is a palindrome', () => {
   expect(palindrome.isArrayPalindrome([3, 9, 4, 4, 9, 3])).toBe(true);
+});
+
+test('expects that [3, 9, 4, 9, 3] is a palindrome', () => {
+  expect(palindrome.isArrayPalindrome([3, 9, 4, 9, 3])).toBe(true);
 });
 
 test('expects that [3, 9, 4, 4, 6, 3] is not a palindrome', () => {

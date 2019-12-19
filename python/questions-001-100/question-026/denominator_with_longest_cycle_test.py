2481cd6c70ab2218f3ede49f87863a5cd27a6630
Dave Maynard <davemaynard24@gmail.com>
2019-12-19 16:46:00 -0500

    updated test suites for solution 26

---
+++ python/questions-001-100/question-026/denominator_with_longest_cycle_test.py
@@ -0,0 +1,19 @@
+import unittest, denominator_with_longest_cycle
+
+
+class TestGetDenominatorWithLongestCycleOne(unittest.TestCase):
+  def test_get_denominator_with_longest_cycle(self):
+    self.assertEqual(
+        denominator_with_longest_cycle.get_denominator_with_longest_cycle(10),
+        7)
+
+
+class TestGetDenominatorWithLongestCycleTwo(unittest.TestCase):
+  def test_get_denominator_with_longest_cycle(self):
+    self.assertEqual(
+        denominator_with_longest_cycle.get_denominator_with_longest_cycle(
+            1000), 983)
+
+
+if __name__ == '__main__':
+  unittest.main()

496b4dcd1a9432f7a78b2b487f605dc0565f1520
Dave Maynard <davemaynard24@gmail.com>
2019-12-19 10:15:13 -0500

    added test suite for solution 25 (python)

---
+++ python/questions-001-100/question-025/fibonacci_term_test.py
@@ -0,0 +1,25 @@
+import unittest, fibonacci_term
+
+
+class TestGetFibonacciTermOne(unittest.TestCase):
+  def test_get_fibonacci_term(self):
+    self.assertEqual(fibonacci_term.get_fibonacci_term(2), 7)
+
+
+class TestGetFibonacciTermTwo(unittest.TestCase):
+  def test_get_fibonacci_term(self):
+    self.assertEqual(fibonacci_term.get_fibonacci_term(3), 12)
+
+
+class TestGetFibonacciTermThree(unittest.TestCase):
+  def test_get_fibonacci_term(self):
+    self.assertEqual(fibonacci_term.get_fibonacci_term(4), 17)
+
+
+class TestGetFibonacciTermFour(unittest.TestCase):
+  def test_get_fibonacci_term(self):
+    self.assertEqual(fibonacci_term.get_fibonacci_term(1000), 4782)
+
+
+if __name__ == '__main__':
+  unittest.main()

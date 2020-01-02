c7027a8f36db37d1836e8b54cb1b07c9e140f535
Dave Maynard <davemaynard24@gmail.com>
2020-01-02 11:57:17 -0500

    ported solution 36 to python

---
+++ python/questions-001-100/question-036/palindromic_sum.py
@@ -0,0 +1,26 @@
+# Double-base palindromes
+
+# Problem 36
+# The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+# Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+# (Please note that the palindromic number, in either base, may not include leading zeros.)
+
+import sys, os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_dir)
+
+from python.util import palindrome
+
+
+def get_palindromic_sum(limit):
+	palindromic_sum = 0
+
+	for i in range(1, limit, 2):
+		if palindrome.is_array_palindrome(list(str(i))):
+			binary = bin(i)[2:]
+			if palindrome.is_array_palindrome(list(binary)):
+				palindromic_sum += i
+
+	return palindromic_sum
---
+++ python/questions-001-100/question-036/palindromic_sum_test.py
@@ -0,0 +1,10 @@
+import unittest, palindromic_sum
+
+
+class TestGetPalindromicSum(unittest.TestCase):
+	def test_get_palindromic_sum(self):
+		self.assertEqual(palindromic_sum.get_palindromic_sum(1000000), 872187)
+
+
+if __name__ == '__main__':
+	unittest.main()

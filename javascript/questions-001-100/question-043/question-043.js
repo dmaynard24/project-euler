06b180bec16fd4b886e0cc90ce86cfd5f661f3e8
Dave Maynard <davemaynard24@gmail.com>
2020-01-07 17:33:31 -0500

    removed unncessary console logs

--- javascript/questions-001-100/question-043/question-043.js
+++ javascript/questions-001-100/question-043/question-043.js
@@ -56,7 +56,6 @@ function getSubstringPandigitalSum() {
   // get remaining first digit of each pandigital, add them all up
   let sum = 0;
   pandigitals.forEach(p => {
-    console.log(getRemainingDigit(p) + p);
     sum += parseInt(getRemainingDigit(p) + p, 10);
   });
 
@@ -94,7 +93,5 @@ function getRemainingDigit(str) {
     }
   }
 }
-
-console.log(getSubstringPandigitalSum());
 
 module.exports = getSubstringPandigitalSum;

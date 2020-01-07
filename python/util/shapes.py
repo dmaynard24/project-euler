fb3d43cbdfa823ce08a5344a093a3a9b916d599d
Dave Maynard <davemaynard24@gmail.com>
2020-01-06 23:21:49 -0500

    wrote higher-order functions to port my shapes utility module from JS to python

---
+++ python/util/shapes.py
@@ -0,0 +1,39 @@
+import math
+
+
+def is_pentagonal(num, pentagons):
+  if pentagons is not None and num in pentagons:
+    return pentagons[num]
+
+  return (math.sqrt(24 * num + 1) + 1) % 6 == 0
+
+
+def get_shapes(limit, get_nth_shape_fn):
+  shapes = {}
+  n = 1
+  term = 1
+
+  while term < limit:
+    shapes[term] = True
+    term = get_nth_shape_fn(n)
+
+    n += 1
+
+  return shapes
+
+
+get_nth_triangle = lambda n: (n / 2) * (n + 1)
+get_nth_square = lambda n: n * n
+get_nth_pentagon = lambda n: (n * (3 * n - 1)) / 2
+get_nth_hexagon = lambda n: n * (2 * n - 1)
+get_nth_heptagon = lambda n: (n * (5 * n - 3)) / 2
+get_nth_octagon = lambda n: n * (3 * n - 2)
+
+get_triangles = lambda limit: get_shapes(limit, get_nth_triangle)
+get_squares = lambda limit: get_shapes(limit, get_nth_square)
+get_pentagons = lambda limit: get_shapes(limit, get_nth_pentagon)
+get_hexagons = lambda limit: get_shapes(limit, get_nth_hexagon)
+get_heptagons = lambda limit: get_shapes(limit, get_nth_heptagon)
+get_octagons = lambda limit: get_shapes(limit, get_nth_octagon)
+
+print(get_nth_square(5))

7ed0e4ceb34cdb202f3082a0655bc28732b04eb1
Dave Maynard <davemaynard24@gmail.com>
2020-01-12 13:06:32 -0500

    finished porting combination module to python

--- python/util/combination.py
+++ python/util/combination.py
@@ -1,4 +1,7 @@
 def get_combos(arr, pick):
+  if pick == 0:
+    return [[]]
+
   if len(arr) < 1:
     return []
 
@@ -6,10 +9,8 @@ def get_combos(arr, pick):
   rest = arr[1:]
 
   next_combos = get_combos(rest, pick - 1)
-  for c in next_combos:
-    c.insert(0, first)
+  # essentially map()
+  for combo in next_combos:
+    combo.insert(0, first)
 
   return next_combos + get_combos(rest, pick)
-
-
-print(get_combos([1, 2, 3], 2))

87d7e07474e9e5ef2046e80c57f7b07997bcb18a
Dave Maynard <davemaynard24@gmail.com>
2020-01-17 17:35:50 -0500

    ported solution 57 to python

---
+++ python/questions-001-100/question-057/fraction_count.py
@@ -0,0 +1,38 @@
+# Square root convergents
+
+# Problem 57
+# It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+
+# √ 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+
+# By expanding this for the first four iterations, we get:
+
+# 1 + 1/2 = 3/2 = 1.5
+# 1 + 1/(2 + 1/2) = 7/5 = 1.4
+# 1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+# 1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+
+# The next three expansions are 99/70, 239/169, and 577/408, but the eighth expansion, 1393/985, is the first example where the number of digits in the numerator exceeds the number of digits in the denominator.
+
+# In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?
+
+import sys, os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_dir)
+
+from python.util import digits
+
+
+def get_fraction_count(iteration_count):
+  count = 0
+  n = 1
+  d = 1
+
+  for i in range(1, iteration_count + 1):
+    n = n + d * 2
+    d = n - d
+
+    if digits.get_digit_count(n) > digits.get_digit_count(d):
+      count += 1
+
+  return count
---
+++ python/questions-001-100/question-057/fraction_count_test.py
@@ -0,0 +1,12 @@
+import unittest, fraction_count
+
+
+class TestGetFractionCount(unittest.TestCase):
+  def test_get_fraction_count(self):
+    self.assertEqual(fraction_count.get_fraction_count(7), 0)
+    self.assertEqual(fraction_count.get_fraction_count(8), 1)
+    self.assertEqual(fraction_count.get_fraction_count(1000), 153)
+
+
+if __name__ == '__main__':
+  unittest.main()

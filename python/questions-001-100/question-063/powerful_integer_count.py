92ab6403e03d0600a651dc6ba5567e4d2159ea5e
Dave Maynard <davemaynard24@gmail.com>
2020-01-22 10:58:40 -0500

    ported solution 63 to python

---
+++ python/questions-001-100/question-063/powerful_integer_count.py
@@ -0,0 +1,30 @@
+# Powerful digit counts
+
+# Problem 63
+# The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the 9-digit number, 134217728=8^9, is a ninth power.
+
+# How many n-digit positive integers exist which are also an nth power?
+
+import sys, os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_dir)
+
+from python.util import digits
+
+
+def get_powerful_integer_count():
+  count = 0
+
+  for exp in range(1, 22):
+    base = 1
+    digit_count = 1
+    while digit_count <= exp:
+      powerful = base**exp
+
+      digit_count = digits.get_digit_count(powerful)
+      if digit_count == exp:
+        count += 1
+
+      base += 1
+
+  return count
---
+++ python/questions-001-100/question-063/powerful_integer_count_test.py
@@ -0,0 +1,10 @@
+import unittest, powerful_integer_count
+
+
+class TestGetPowerfulIntegerCount(unittest.TestCase):
+  def test_get_powerful_integer_count(self):
+    self.assertEqual(powerful_integer_count.get_powerful_integer_count(), 49)
+
+
+if __name__ == '__main__':
+  unittest.main()

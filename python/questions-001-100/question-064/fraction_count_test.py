fb501e0259645c1c792517b544348502307ebe55
Dave Maynard <davemaynard24@gmail.com>
2020-01-22 11:21:04 -0500

    ported solution 64 to python

---
+++ python/questions-001-100/question-064/fraction_count_test.py
@@ -0,0 +1,10 @@
+import unittest, fraction_count
+
+
+class TestGetPowerfulIntegerCount(unittest.TestCase):
+  def test_get_fraction_count(self):
+    self.assertEqual(fraction_count.get_fraction_count(10_000), 1_322)
+
+
+if __name__ == '__main__':
+  unittest.main()

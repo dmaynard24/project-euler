2c167a466bd88389d0268b080134cf13339630f3
Dave Maynard <davemaynard24@gmail.com>
2020-02-03 08:20:56 -0500

    ported phi utility module to python

---
+++ python/util/phi.py
@@ -0,0 +1,15 @@
+from . import primal
+
+
+def phi(val, primes):
+  if primal.is_prime(val, primes):
+    return val - 1
+
+  prime_factors = primal.get_prime_factors(val, primes)
+  multiplier = 1
+  for prime_factor in prime_factors:
+    multiplier *= (1 - 1 / prime_factor['base'])
+
+  count = val * multiplier
+
+  return count

54b53aa564446243b7f9cb8d04f12733f1f4d915
Dave Maynard <davemaynard24@gmail.com>
2020-02-06 13:10:05 -0500

    ported solution 77 to python

---
+++ python/questions_001_100/question_077/prime_summation_count.py
@@ -0,0 +1,52 @@
+# Prime summations
+
+# Problem 77
+# It is possible to write ten as the sum of primes in exactly five different ways:
+
+# 7 + 3
+# 5 + 5
+# 5 + 3 + 2
+# 3 + 3 + 2 + 2
+# 2 + 2 + 2 + 2 + 2
+
+# What is the first value which can be written as the sum of primes in over five thousand different ways?
+
+import sys, os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_dir)
+
+from python.util import primal
+
+
+def get_prime_summation_count(combo_count):
+  primes = primal.get_primes(100)
+  prime_nums_reversed = primal.get_prime_numbers(primes)
+  prime_nums_reversed.reverse()
+
+  def get_count(remaining, index):
+    if remaining == 0:
+      return 1
+
+    count = 0
+    for i in range(index, len(prime_nums_reversed)):
+      next_term = prime_nums_reversed[i]
+      new_remaining = remaining - next_term
+      next_index = i if new_remaining >= next_term else i + 1
+      count += get_count(new_remaining, next_index)
+
+    return count
+
+  prime_sum = 10
+  count = 5
+  first_index = None
+  while count < combo_count:
+    prime_sum += 1
+
+    for i in range(len(prime_nums_reversed) - 1, -1, -1):
+      if prime_nums_reversed[i] >= prime_sum:
+        first_index = i + 1
+        break
+
+    count = get_count(prime_sum, first_index)
+
+  return prime_sum
---
+++ python/questions_001_100/question_077/prime_summation_count_test.py
@@ -0,0 +1,12 @@
+import unittest, prime_summation_count
+
+
+class TestGetPrimeSummationCount(unittest.TestCase):
+  def test_get_prime_summation_count(self):
+    self.assertEqual(prime_summation_count.get_prime_summation_count(5), 10)
+    self.assertEqual(prime_summation_count.get_prime_summation_count(5_000),
+                     71)
+
+
+if __name__ == '__main__':
+  unittest.main()

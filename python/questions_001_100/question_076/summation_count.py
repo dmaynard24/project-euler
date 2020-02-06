2dd462a0229268f1340fd23ca58fa326bc1acc54
Dave Maynard <davemaynard24@gmail.com>
2020-02-06 09:37:31 -0500

    ported solution 76 to python

---
+++ python/questions_001_100/question_076/summation_count.py
@@ -0,0 +1,50 @@
+# Counting summations
+
+# Problem 76
+# It is possible to write five as a sum in exactly six different ways:
+
+# 4 + 1
+# 3 + 2
+# 3 + 1 + 1
+# 2 + 2 + 1
+# 2 + 1 + 1 + 1
+# 1 + 1 + 1 + 1 + 1
+
+# How many different ways can one hundred be written as a sum of at least two positive integers?
+
+
+def get_summation_count(num):
+  # keep a cache of counts to avoid redundant recursion
+  summation_counts = {}
+
+  def get_count(remaining, term):
+    if remaining == 0:
+      return 1
+
+    count = 0
+    for next_term in range(term, 0, -1):
+      new_remaining = remaining - next_term
+      next_term_arg = min(new_remaining, next_term)
+      cached_addend = None
+      addend = None
+
+      # check cache
+      if summation_counts.get(new_remaining) is not None:
+        # check chained dict
+        if summation_counts[new_remaining].get(next_term_arg) is not None:
+          cached_addend = summation_counts[new_remaining][next_term_arg]
+      else:
+        summation_counts[new_remaining] = {}
+
+      # wasn't cached, calculate addend and cache it
+      if cached_addend is None:
+        addend = get_count(new_remaining, next_term_arg)
+        summation_counts[new_remaining][next_term_arg] = addend
+      else:
+        addend = cached_addend
+
+      count += addend
+
+    return count
+
+  return get_count(num, num - 1)
---
+++ python/questions_001_100/question_076/summation_count_test.py
@@ -0,0 +1,11 @@
+import unittest, summation_count
+
+
+class TestGetSummationCount(unittest.TestCase):
+  def test_get_summation_count(self):
+    self.assertEqual(summation_count.get_summation_count(5), 6)
+    self.assertEqual(summation_count.get_summation_count(100), 190_569_291)
+
+
+if __name__ == '__main__':
+  unittest.main()

ee39ea54427b5225422864c8757afcd684fbb48b
Dave Maynard <davemaynard24@gmail.com>
2020-02-18 20:50:32 -0500

    started refactoring question 78 with a generating function

--- javascript/questions-001-100/question-078/question-078.js
+++ javascript/questions-001-100/question-078/question-078.js
@@ -12,66 +12,16 @@
 // O   O   O   O   O
 // Find the least value of n for which p(n) is divisible by one million.
 
+const shapes = require('../../util/shapes');
+
 function getLeastN(divisor) {
   // use mod later to prevent big ints
   let mod = divisor * 10;
 
-  // keep a cache of counts to avoid redundant recursion
-  let partitionCounts = new Map();
-
-  function getPartitionCount(remaining, term) {
-    if (remaining == 0) {
-      return 1;
-    }
-
-    let count = 0;
-    for (let nextTerm = term; nextTerm > 0; nextTerm--) {
-      let newRemaining = remaining - nextTerm,
-        nextTermArg = Math.min(newRemaining, nextTerm),
-        cachedAddend,
-        addend = 0;
-
-      // check cache
-      if (partitionCounts.has(newRemaining)) {
-        // check chained map
-        if (partitionCounts.get(newRemaining).has(nextTermArg)) {
-          cachedAddend = partitionCounts.get(newRemaining).get(nextTermArg);
-        }
-      } else {
-        partitionCounts.set(newRemaining, new Map());
-      }
-
-      if (!cachedAddend) {
-        addend = getPartitionCount(newRemaining, nextTermArg);
-        partitionCounts.get(newRemaining).set(nextTermArg, addend);
-      } else {
-        addend = cachedAddend;
-      }
-
-      count += addend;
-      if (count > mod) {
-        count %= mod;
-      }
-    }
-
-    return count;
-  }
-
-  let i = 1,
-    limit = 20000;
-  while (i < limit) {
-    let partitionCount = getPartitionCount(i, i);
-    if (partitionCount % divisor == 0) {
-      console.log(i, partitionCount, divisor);
-      return i;
-    }
-    i++;
-  }
-
   return `couldn't find a solution for n in p(n) where n is less than ${limit}`;
 }
 
-console.log(getLeastN(10000));
+console.log(getLeastN(1000));
 // 100000 - 11224
 // 10000 - 599
 // 1000 - 449

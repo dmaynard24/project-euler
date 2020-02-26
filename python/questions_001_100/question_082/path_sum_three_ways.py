11fffa71eade38dc3831fa0eeed2e1b89667a64c
Dave Maynard <davemaynard24@gmail.com>
2020-02-26 18:05:00 -0500

    setting up question 82

---
+++ python/questions_001_100/question_082/path_sum_three_ways.py
@@ -0,0 +1,20 @@
+# Path sum: three ways
+
+# Problem 82
+# NOTE: This problem is a more challenging version of Problem 81.
+
+# The minimal path sum in the 5 by 5 matrix below, by starting in any cell in the left column and finishing in any cell in the right column, and only moving up, down, and right, is indicated in red and bold; the sum is equal to 994.
+
+# Find the minimal path sum from the left column to the right column in matrix, a string containing an 80 by 80 matrix.
+
+matrix = '''
+131 673 234 103 18
+201 96 342 965 150
+630 803 746 442 111
+537 699 497 121 956
+805 732 524 37 131
+'''
+
+
+def minimal_path_sum():
+  return 994

846d531f0ac46053190d852dc67c55b71651bc1f
Dave Maynard <davemaynard24@gmail.com>
2020-04-05 16:41:15 -0400

    solved problem 85

---
+++ python/questions_001_100/question_085/counting_rectangles.py
@@ -0,0 +1,41 @@
+# Counting rectangles
+
+# Problem 85
+# By counting carefully it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles.
+
+# Although there exists no rectangular grid that contains exactly two million rectangles, find the area of the grid with the nearest solution.
+
+import sys
+
+
+def get_rect_count(rect_w, rect_h):
+  count = 0
+
+  for w in range(1, rect_w + 1):
+    for h in range(1, rect_h + 1):
+      count += ((rect_w - w) + 1) * ((rect_h - h) + 1)
+
+  return count
+
+
+def get_closest_area(target_count):
+  closest_area = None
+  smallest_diff = sys.maxsize
+
+  # max our width at 100
+  max_w = min(target_count // 2, 100)
+
+  for w in range(3, max_w):
+    for h in range(2, w + 1):
+      rect_count = get_rect_count(w, h)
+      diff = abs(target_count - rect_count)
+      if diff < smallest_diff:
+        closest_area = w * h
+        smallest_diff = diff
+
+  return closest_area
+
+
+print(get_closest_area(18))  # 6
+print(get_closest_area(60))  # 12
+print(get_closest_area(2_000_000))  # 2772

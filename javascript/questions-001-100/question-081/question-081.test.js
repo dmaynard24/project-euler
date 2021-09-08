610e3621a86b06eedb9780346d7306b8dbb333f2
Dave Maynard <dmayna@amazon.com>
2021-09-08 17:06:07 -0400

    removed unused comment, empty __init__.py file

--- javascript/questions-001-100/question-081/question-081.test.js
+++ javascript/questions-001-100/question-081/question-081.test.js
@@ -1,4 +1,3 @@
-// const getMinimalPathSum = require('./queston-081');
 const getMinimalPathSum = require(`./question-081`);
 
 test(`gets the minimal path sum in the matrix string containing a 80 by 80 matrix, from the top left to the bottom right by only moving right and down to be 427,337`, () => {
